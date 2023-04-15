33c8d72ca02e17f65e8834285541a663ce2aefac
Xern <std.sonnguyen@gmail.com>
2023-04-15 17:03:59 +0700

    remove random attributes

--- user_info.py
+++ user_info.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 from Update import Update
-import random
-from lorem_text import lorem
+import requests
 
 
 class UserInfo:
-    def __init__(self, window):
+    def __init__(self, window, cursor, update_user_icon_callback=None):
         self.window = window
-
+        self.cursor = cursor
+        self.update_user_icon_callback = update_user_icon_callback
+        
         # self.user_data = {
         #     'avatar': 'images/orgasm.jpg',
         #     'username': 'Xern',
@@ -24,7 +25,7 @@ class UserInfo:
 
         # if user_data is None:
         user_data = {
-            'avatar': 'images/orgasm.jpg',
+            'avatar': 'images/guest.png',
             'username': None,
             'email': None,
             'age': None,
@@ -51,8 +52,11 @@ class UserInfo:
         self.user_info_window.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
         # User's avatar
-        avatar_image = Image.open(self.user_data['avatar'])
-        avatar_image = avatar_image.resize((80, 80), Image.Resampling.LANCZOS)
+        if self.user_data['avatar'] == 'images/guest.png':
+            avatar_image = Image.open(self.user_data['avatar'])
+        else:
+            avatar_image = Image.open(requests.get(self.user_data['avatar'], stream=True).raw)
+        avatar_image = avatar_image.resize((100, 100), Image.Resampling.LANCZOS)
         avatar_image_button = ImageTk.PhotoImage(avatar_image)
 
         avatar_label = tk.Label(self.user_info_window, image=avatar_image_button)
@@ -62,31 +66,54 @@ class UserInfo:
         # User's info
         username_label = tk.Label(self.user_info_window, text=f"Username: {self.user_data['username']}")
         username_label.grid(row=0, column=1, sticky=tk.W, pady=(10, 0))
+        
+        email_label = tk.Label(self.user_info_window, text=f"Email: {self.user_data['email']}")
+        email_label.grid(row=1, column=1, sticky=tk.W)
 
         age_label = tk.Label(self.user_info_window, text=f"Age: {self.user_data['age']}")
-        age_label.grid(row=1, column=1, sticky=tk.W)
+        age_label.grid(row=2, column=1, sticky=tk.W)
 
         user_role_label = tk.Label(self.user_info_window, text=f"User Role: {self.user_data['user_role']}")
-        user_role_label.grid(row=2, column=1, sticky=tk.W)
+        user_role_label.grid(row=3, column=1, sticky=tk.W)
 
         favorite_label = tk.Label(self.user_info_window, text=f"Favorite: {self.user_data['favorite']}")
-        favorite_label.grid(row=3, column=1, sticky=tk.W)
+        favorite_label.grid(row=4, column=1, sticky=tk.W)
 
-        comics_followed_label = tk.Label(self.user_info_window,
-                                         text=f"Comics followed: {self.user_data['comics_followed']}")
-        comics_followed_label.grid(row=4, column=1, sticky=tk.W)
+        comics_followed_label = tk.Label(self.user_info_window, text=f"Comics followed: {self.user_data['comics_followed']}")
+        comics_followed_label.grid(row=5, column=1, sticky=tk.W)
 
         # Add a new column and configure it to expand
         self.user_info_window.columnconfigure(2, weight=1)
 
         # Button
         button = tk.Button(self.user_info_window, text="Update", command=self.update)
-        button.grid(row=8, column=1, pady=(20, 10), sticky=tk.E + tk.W)
+        button.grid(row=8, column=1, pady=(40, 20), sticky=tk.E + tk.W)
         button.configure(anchor='center')
 
-
-
     def update(self):
-        Update(self.user_info_window)
+        update_window = Update(self.user_info_window)
+        update_window.update_data_callback = self.update_user_data
+
+    def update_user_data(self, age, favorite, avatar):
+        self.user_data['age'] = age
+        self.user_data['favorite'] = favorite
+        self.user_data['avatar'] = avatar
+
+        user_data = {
+            "age": self.user_data['age'],
+            "favorite": self.user_data['favorite'],
+            "avatar": self.user_data['avatar'],
+            "username": self.user_data['username']
+        }
+        values = tuple(user_data.values())
+
+        self.cursor.execute("UPDATE users SET age=%s, favorite=%s, avatar=%s WHERE username=%s", values)
+
+        # Call the update_user_icon method
+        if self.update_user_icon_callback:
+            self.update_user_icon_callback()
+
+    def set_update_user_icon_callback(self, callback):
+        self.update_user_icon_callback = callback
 
 
