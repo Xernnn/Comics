bc230aa69e9083ca1bb8cacd98fb8f6f4da28300
Xern <std.sonnguyen@gmail.com>
2023-04-18 01:44:57 +0700

    add followed_comics

--- user.sql
+++ user.sql
@@ -1,23 +1,22 @@
 CREATE SCHEMA IF NOT EXISTS comics;
+-- drop schema comics;
 use comics;
 CREATE TABLE users (
-	username varchar(255),
+    username varchar(255) NOT NULL,
     password varchar (255),
     avatar text,
     gmail varchar(255),
     role varchar (255),
     age int, 
     favorite varchar(255),
-    comics_followed int,
-    PRIMARY KEY (
-        username
-    )
+    comics_followed text,
+    PRIMARY KEY (username)
 );
 select * from users;
 -- drop table users;
 
 CREATE TABLE comics (
-	title varchar(255),
+    title varchar(255) NOT NULL,
     author varchar(255),
     artist varchar(255),
     publisher varchar(255),
@@ -28,12 +27,20 @@ CREATE TABLE comics (
     cover_image text,
     language varchar(255), 
     synopsis text,
-    PRIMARY KEY (
-        title
-    )
+    PRIMARY KEY (title)
 );
+
 select * from comics;
-drop table comics;
+-- drop table comics;
+
+CREATE TABLE followed_comics (
+	numlist int AUTO_INCREMENT,
+    title varchar(255),
+    username varchar(255),
+    PRIMARY KEY (numlist)
+);
+select * from followed_comics;
+drop table followed_comics;
 
 CREATE TABLE sort (
 	order_by varchar(255),
